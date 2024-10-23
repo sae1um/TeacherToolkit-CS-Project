86d24440b294d5567724ff91637799a0a449f422
Godswill Erhunmwunse <113043388+sae1um@users.noreply.github.com>
2024-10-23 19:06:00 +0100

    fixed list not updating

--- client/src/pages/Toolkit/ToolkitStudentPickerPage.js
+++ client/src/pages/Toolkit/ToolkitStudentPickerPage.js
@@ -4,53 +4,57 @@ import { TeacherClasses } from "../../helpers/TestClasses"
 export default function ToolkitStudentPicker() {
     const [allClasses, setAllClasses] = useState([]);
     const [selectedClass, setSelectedClass] = useState(TeacherClasses[0]);
-    const [studentList, setStudentList] = useState([]);
+    // const [studentList, setStudentList] = useState([]);
 
-    // Sets teachers classes from db
+    // Sets teachers classes from db 
     // Sets it to the variable allClasses to put in list
     useEffect(() => {
         setAllClasses(TeacherClasses);
     }, []);
     //Shows which class was selected (For debugging)
     useEffect(() => {
-        if(selectedClass){
+        if (selectedClass) {
             console.log("Selected class changed to:", selectedClass, selectedClass.students);
         }
-    }, [selectedClass])
+    }, [selectedClass]);
 
     // Sets selectedClass to the one chosen in dropdown
-    function getSelectClass(e){
+    function getSelectClass(e) {
         const selectedClassName = e.target.value;
-        const foundClass = allClasses.find(itemClass=> itemClass.name === selectedClassName);
+        const foundClass = allClasses.find(itemClass => itemClass.name === selectedClassName);
         setSelectedClass(foundClass);
     }
 
     //Class Dropdown Selecter => Students of that classes are loaded up => Remove students from class => randomise => more?
-    return(
+    return (
         <div className="flex h-full justify-center">
             <div className="flex flex-col items-center w-1/2 h- mt-4 p-4 bg-white shadow-lg rounded-lg gap-2">
                 <h3 className="font-bold text-2xl">Random Student Picker</h3>
                 <select onChange={getSelectClass}>
                     {/* Choice of the teachers class to choose from */}
                     {
-                        allClasses.length > 0 ? 
+                        allClasses.length > 0 ?
                             allClasses.map((singleClass) => (
                                 <option value={singleClass.name} key={singleClass.teacher}>
                                     {singleClass.name}
                                 </option>
                             ))
-                        :
-                        <option value={null}>No class</option>
-                    }   
+                            :
+                            <option value={null}>No class</option>
+                    }
                 </select>
-                <div className="flex flex-col items-center justify-center mt-2">
-                    <h4 className="font-bold text-xl">Students</h4>
+                <div className="flex flex-row items-center justify-center mt-2">
                     {/* List of students from the selected class to pick from */}
-                    <div className="flex flex-col gap-2">
+                    <div className="flex items-center justify-center flex-col">
+                    <h4 className="font-bold text-xl">Students</h4>
                         {
-                            selectedClass && selectedClass.students.map((student) => (
-                                <span key={selectedClass.name}>{student}</span>
-                            ))
+                            selectedClass && (
+                                <ul className="flex flex-col gap-2">
+                                    {selectedClass.students.map((student, index) => (
+                                        <li key={index} className="text-center">{student}</li>
+                                    ))}
+                                </ul>
+                            )
                         }
                     </div>
                 </div>
@@ -59,8 +63,8 @@ export default function ToolkitStudentPicker() {
     )
 };
 
-function StudentItem({classStudents}){
-    return(
+function StudentItem({ classStudents }) {
+    return (
         <div>
             Names
         </div>
